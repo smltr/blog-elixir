5eb85fc2ca482336a6fc71c40fe7d651e836b8b6
Sam Trouy <trouys16@gmail.com>
2024-09-20 11:40:17 -0400

    remove unused user auth files

--- lib/samt_web/router.ex
+++ lib/samt_web/router.ex
@@ -8,7 +8,6 @@ defmodule SamtWeb.Router do
     plug :put_root_layout, html: {SamtWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug :fetch_current_user
   end
 
   pipeline :api do
